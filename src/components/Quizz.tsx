eee21fc3d8673f4e8198d00a8f9c8ce037c6967f
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2023-11-15 14:44:47 +0100

    feat: the name of the flag is encoded

--- src/components/Quizz.tsx
+++ src/components/Quizz.tsx
@@ -2,14 +2,15 @@ import { QA } from "../interfaces/types";
 import AnswerComponent from "./Answer";
 import GenerateQA from "../utils/GenerateQA";
 import "./Quizz.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function QuizzComponent() {
 
-	const [Round, setRound] = useState<QA>(GenerateQA());
+	const [round, setRound] = useState<QA>(GenerateQA());
+	const [encodedImage, setEncodedImage] = useState<string>("");
 
 	function handleSelection(index: number) {
-		if (index === Round.answers.correctIndex) {
+		if (index === round.answers.correctIndex) {
 			alert("Correct!");
 		}
 		else {
@@ -18,14 +19,27 @@ export default function QuizzComponent() {
 		setTimeout(() => {setRound(GenerateQA)}, 200);
 	}
 
+	useEffect(() => {
+		// Load the SVG image using an XMLHttpRequest
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", round.flag);
+		xhr.onload = () => {
+			// Encode the response in Base64 format
+			const encoded = btoa(xhr.responseText);
+			setEncodedImage(`data:image/svg+xml;base64,${encoded}`);
+		};
+		xhr.send();
+	}, [round.flag]);
+
 	return (
 		<div id="quizz-wrapper">
-			<img src={Round.flag} alt="photo" />
+			{/* Use the encoded image in the img tag */}
+			<img src={encodedImage} alt="photo" />
 			<div id="answer-wrapper">
-				<AnswerComponent answer={Round.answers.answer0} select={() => handleSelection(0)} index={0} />
-				<AnswerComponent answer={Round.answers.answer1} select={() => handleSelection(1)} index={1} />
-				<AnswerComponent answer={Round.answers.answer2} select={() => handleSelection(2)} index={2} />
-				<AnswerComponent answer={Round.answers.answer3} select={() => handleSelection(3)} index={3} />
+				<AnswerComponent answer={round.answers.answer0} select={() => handleSelection(0)} index={0} />
+				<AnswerComponent answer={round.answers.answer1} select={() => handleSelection(1)} index={1} />
+				<AnswerComponent answer={round.answers.answer2} select={() => handleSelection(2)} index={2} />
+				<AnswerComponent answer={round.answers.answer3} select={() => handleSelection(3)} index={3} />
 			</div>
 		</div>
 	);
