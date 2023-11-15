93dbf633111cddd46ee6361b2a5fe87b04a3317f
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2023-11-15 11:38:47 +0100

    start of header and quizz

---
+++ src/assets/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function HeaderComponent() {
+	return (
+	    <div>
+			<h1 id="header-h1">🌍Geo-Quizz🌎</h1>
+		</div>
+	);
+}
---
+++ src/assets/components/Quizz.tsx
@@ -0,0 +1,9 @@
+import "./Quizz.scss";
+export default function QuizzComponent(){
+    return (
+        <div id="quizz-wrapper">
+            <img src="https://picsum.photos/350" alt="photo" />
+        </div>
+    );
+}
+
