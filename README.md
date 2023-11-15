f25099430276730043bd106042d005a265c663f0
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2023-11-15 17:21:14 +0100

    build + updated the path to svg : "../public/svg" becomes "../svg"

--- README.md
+++
@@ -1,3 +0,0 @@
-# Geo-quizz
-
-Flags of the world from this repo: https://github.com/hampusborgos/country-flags
--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function HeaderComponent() {
-	return (
-	    <div id="header">
-			<h1 >🌍Geo-Quizz🌎</h1>
-			<h3>10 seconds to identify the most flags!</h3>
-		</div>
-	);
-}
