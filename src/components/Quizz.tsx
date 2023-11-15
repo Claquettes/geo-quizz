994bfe684b5db4a6db5b51e2b861637f95b7d02c
Claquettes <mathieu.ponton@etu.univ-lyon1.fr>
2023-11-15 14:41:54 +0100

    feat: using useState for the infos for the round to easily generate news

--- src/components/Quizz.tsx
+++ src/components/Quizz.tsx
@@ -2,15 +2,20 @@ import { QA } from "../interfaces/types";
 import AnswerComponent from "./Answer";
 import GenerateQA from "../utils/GenerateQA";
 import "./Quizz.scss";
+import { useState } from "react";
 
 export default function QuizzComponent() {
 
-	const Round: QA = GenerateQA();
+	const [Round, setRound] = useState<QA>(GenerateQA());
 
 	function handleSelection(index: number) {
 		if (index === Round.answers.correctIndex) {
 			alert("Correct!");
 		}
+		else {
+			alert("Wrong!");
+		}
+		setTimeout(() => {setRound(GenerateQA)}, 200);
 	}
 
 	return (
@@ -22,7 +27,6 @@ export default function QuizzComponent() {
 				<AnswerComponent answer={Round.answers.answer2} select={() => handleSelection(2)} index={2} />
 				<AnswerComponent answer={Round.answers.answer3} select={() => handleSelection(3)} index={3} />
 			</div>
-			<p>{Round.answers.correctIndex}</p>
 		</div>
 	);
 }
